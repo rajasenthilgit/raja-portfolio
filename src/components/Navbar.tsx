cb56692e3bd7b659658e5954ab4fc5fb0047a02f
Rajasenthilkumar_github <raja2001ece@gmail.com>
2026-06-01 11:59:52 +0530

    feat: update resume 1

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -54,7 +54,7 @@ export default function Navbar() {
   // Function to download resume
   const downloadResume = () => {
     const link = document.createElement('a');
-    link.href = '/Raja_Resume.pdf.pdf';
+    link.href = '/Raja_Resume.pdf';
     link.download = 'Raja_Senthilkumar_Resume.pdf';
     document.body.appendChild(link);
     link.click();
